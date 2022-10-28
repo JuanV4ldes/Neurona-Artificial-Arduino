c0386d4e9ba40a9e911180c626a8bc160bb0f7b4
Juan Valdés <67657234+JuanV4ldes@users.noreply.github.com>
2022-10-28 20:46:06 +0000

    Delete buscador wiki 1.0 directory

--- buscador wiki 1.0/energy.py
+++
@@ -1,7 +0,0 @@
-#Ley de Coulomb
-
-Q1 = float(input("Valor de la carga 1 (notacion cientifica):     "))
-Q2 = float(input("Valor de la carga 2:     "))
-
-Distancia = float("Valor de la distancia en metros:    ")
-
